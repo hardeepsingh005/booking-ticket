ec0f165ff8cdefb5ce6b039e7f6e285e301a3e83
Hardeep <hardeepsinghguleria5@gmail.com>
2026-07-21 13:05:41 +0530

    create product page and route from header updated

--- swift-book/src/app/Component/Common/Header.jsx
+++ swift-book/src/app/Component/Common/Header.jsx
@@ -76,12 +76,12 @@ export default function Header() {
               >
                 About
               </Link>
-              <a
-                href="#"
+              <Link
+                href="/products"
                 className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"
               >
-                Projects
-              </a>
+                Product
+              </Link>
               <a
                 href="#"
                 className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"
